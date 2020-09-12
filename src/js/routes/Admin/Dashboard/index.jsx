b8f30e6e1c7c1c08314aa33b3b217c96035b94cc
Kalob Porter <kalob@kalob.net>
2020-09-12 16:11:46 -0400

    A little bit of cleanup after initial data implementation

--- src/js/routes/Admin/Dashboard/index.jsx
+++ src/js/routes/Admin/Dashboard/index.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
-
 import Grid from '@material-ui/core/Grid';
-
-import { invites } from 'js/common/models';
-import { list } from 'js/redux/entities';
 
 import Header from '../Header';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(list(invites));
-  }, [])
 
   return (
     <Grid container>
